fd7e1a5a41dc2b3309f7ef265e4bbd7dd5163bf4
Danylo Kondratiev <knightpp@proton.me>
2024-08-31 22:21:54 +0300

    Add tests

--- test/esctg/parser_test.exs
+++ test/esctg/parser_test.exs
@@ -3,8 +3,15 @@ defmodule Esctg.ParserTest do
   alias Esctg.Parser
 
   @data_daifuku File.read!("test/data/daifuku.html")
+  @expect_daifuku File.read!("test/data/daifuku.expect")
+  @data_ogo File.read!("test/data/ogo.html")
+  @expect_ogo File.read!("test/data/ogo.expect")
 
   test "daifuku" do
-    dbg(Parser.parse!(@data_daifuku))
+    assert Parser.parse!(@data_daifuku) == :erlang.binary_to_term(@expect_daifuku)
+  end
+
+  test "ogo" do
+    assert Parser.parse!(@data_ogo) == :erlang.binary_to_term(@expect_ogo)
   end
 end
