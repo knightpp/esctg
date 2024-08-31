b272d86dbb1d933ccb96cac7bc44cde451266b8f
Danylo Kondratiev <knightpp@proton.me>
2024-08-31 11:21:27 +0300

    Implement tg parser

---
+++ lib/esctg/parser.ex
@@ -0,0 +1,80 @@
+defmodule Esctg.Parser do
+  def parse!(html) do
+    doc = Floki.parse_document!(html)
+    [title] = Floki.attribute(doc, "meta[property='og:title'][content]", "content")
+    [image] = Floki.attribute(doc, "meta[property='og:image'][content]", "content")
+    [description] = Floki.attribute(doc, "meta[property='og:description'][content]", "content")
+    msgs = Floki.find(doc, "body main .tgme_widget_message") |> Enum.map(&parse_message!/1)
+
+    %{
+      title: title,
+      image: image,
+      description: description,
+      messages: msgs
+    }
+  end
+
+  defp parse_message!(msg) do
+    text =
+      msg
+      |> Floki.find(".tgme_widget_message_text")
+      |> Floki.text()
+
+    [datetime] = Floki.attribute(msg, "time", "datetime")
+    # datetime = DateTime.from_iso8601(datetime)
+
+    %{
+      id: parse_id!(msg),
+      text: text,
+      media: parse_media!(msg),
+      datetime: datetime
+    }
+  end
+
+  defp parse_id!(msg) do
+    [id] = Floki.attribute(msg, "div.tgme_widget_message", "data-post")
+
+    case :binary.split(id, "/") do
+      [_, id] ->
+        {id, _} = Integer.parse(id)
+        id
+    end
+  end
+
+  defp parse_media!(msg) do
+    # in case of multiple images it will return them all
+    Floki.attribute(msg, "a.tgme_widget_message_photo_wrap", "style")
+    |> Enum.map(&parse_background_image/1)
+  end
+
+  # @pattern :binary.compile_pattern(["\n", ";"])
+  defp parse_background_image(style) do
+    list =
+      String.splitter(style, ["\n", ";"])
+      |> Stream.map(&parse_style_line/1)
+      |> Stream.filter(&(&1 != nil))
+      |> Stream.filter(fn {key, _value} -> key == "background-image" end)
+      |> Stream.map(fn {_key, value} -> parse_css_url(value) end)
+      |> Stream.filter(&(&1 != nil))
+      |> Enum.take(1)
+
+    case list do
+      [url] -> url
+      _ -> nil
+    end
+  end
+
+  defp parse_style_line(line) do
+    case :binary.split(line, ":") do
+      [key, value] -> {key, value}
+      _ -> nil
+    end
+  end
+
+  defp parse_css_url(url) do
+    case Regex.run(~r/url\('(.+)'\)/, url) do
+      [_, group] -> group
+      _ -> nil
+    end
+  end
+end
