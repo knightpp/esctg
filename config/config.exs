dbf75463dfe268d2b5b43feb790ed821cb0152d9
Danylo Kondratiev <knightpp@proton.me>
2024-09-08 16:54:18 +0300

    Set logger level

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,15 @@
 import Config
 
+lvl =
+  if config_env() == :prod do
+    :info
+  else
+    :debug
+  end
+
+config :logger,
+  level: lvl
+
 config :esctg, Esctg.Repo, database: "database.db"
 
 config :esctg,
