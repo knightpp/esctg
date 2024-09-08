2ba417c12148109bdb3a0d181fd3a4cf9f6f1c8e
Danylo Kondratiev <knightpp@proton.me>
2024-09-08 13:38:16 +0300

    Create indexes

--- priv/repo/migrations/20240901100155_create_seen.exs
+++ priv/repo/migrations/20240901100155_create_seen.exs
@@ -6,5 +6,7 @@ defmodule Esctg.Repo.Migrations.CreateSeen do
       add(:post_id, :integer, null: false)
       add(:channel_id, references("channels"), null: false)
     end
+
+    create(unique_index(:seens, [:post_id, :channel_id]))
   end
 end
--- priv/repo/migrations/20240901100434_create_channels.exs
+++ priv/repo/migrations/20240901100434_create_channels.exs
@@ -7,9 +7,14 @@ defmodule Esctg.Repo.Migrations.CreateChannels do
       add(:title, :string, size: 255, null: false)
       add(:image, :string, size: 255, null: false)
       add(:description, :string, size: 255, null: false)
+
       add(:api_token, :string, size: 255, null: false)
       add(:api_url, :string, size: 255, null: false)
       add(:enabled, :boolean, null: false)
+
+      timestamps()
     end
+
+    create(unique_index(:channels, [:url, :enabled]))
   end
 end
